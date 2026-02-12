7f4f73a141124bc832db9fed6ded00b359607cb2
nuriaesquius <nuria.esquius01@estudiant.upf.edu>
2026-02-12 14:06:49 +0100

    save

--- lab2/main.c
+++ lab2/main.c
@@ -8,70 +8,85 @@
 
 #define MAX_LINE 1024
 
-void execute_single(char *line, int wait) {
-    char **args = split_command(line);;
+int read_next_line(CircularBuffer *cb, char *dest, int *reachedEOF) { //1->readed line, 0->no line to read
+    int line_size;
+    while ((line_size = buffer_size_next_element(cb, '\n', *reachedEOF)) <= 0 && !(*reachedEOF)) { //keep reading until we have a line or we reach EOF
+        int c = fgetc(stdin);
+        if (c == EOF) {
+            *reachedEOF = 1;
+        } 
+        else {
+            buffer_push(cb, c);
+        }
+    }
+    if (line_size > 0) {
+        memset(dest, 0, MAX_LINE);
+        for (int i = 0; i < line_size; i++) {
+            dest[i] = buffer_pop(cb);
+        }
+        dest[strcspn(dest, "\r\n")] = '\0'; //remove newline characters
+        return 1; //successfully read a line
+    }
+    return 0; // no line to read
+}
 
-    pid_t pid;
-    pid = fork();
+void execute_single(char *line, int wait_child) {
+    char **args = split_command(line);
+    if (args[0] == NULL) {
+        free(args);
+        return;
+    }
 
+    pid_t pid = fork();
     if (pid == 0) { //child
-        execvp(args[0], args);
-        perror("execvp failed");
+        execvp(args[0], args); //execute the command
+        perror("Error en execvp");
         exit(1);
-    } else if (pid > 0) { //parent
-        if (wait==1){
-            waitpid(pid, NULL, 0);
+    } 
+    else if (pid > 0) { //father
+        if (wait_child) {
+            waitpid(pid, NULL, 0); //wait for the child to finish
         }
         free(args);
-    } else { //fork failed
-        perror("fork failed");
+    } 
+    else {
+        perror("Error en fork");
     }
 }
 
 void execute_piped(char *line1, char *line2) {
-    char **args1;
-    char **args2;
-
-    args1 = split_command(line1); 
-    args2 = split_command(line2);
-
+    char **args1 = split_command(line1);
+    char **args2 = split_command(line2);
     int fd[2];
-    
-    if (pipe(fd) == -1) {
-        perror("pipe failed");
-    return;
-    }
 
+    if (pipe(fd) == -1) {
+        perror("Error en pipe");
+        return;
+    }
 
     pid_t pid1 = fork();
-
     if (pid1 < 0) { 
-        perror("fork failed"); 
+        perror("Error en fork"); 
         return; 
     }
-
-    else if (pid1 == 0) { //child 1
-        dup2(fd[1], STDOUT_FILENO); //stdout to write pipe
+    else if (pid1 == 0) {
+        dup2(fd[1], STDOUT_FILENO); //stdout of child 1 goes to write pipe
         close(fd[0]);
         close(fd[1]);
-        execvp(args1[0], args1);
-        perror("execvp failed");
+        execvp(args1[0], args1); //execute first command
         exit(1);
     }
 
     pid_t pid2 = fork();
-
     if (pid2 < 0) { 
-        perror("fork failed"); 
+        perror("Error en fork"); 
         return; 
-    } 
-
+    }
     else if (pid2 == 0) {
-        dup2(fd[0], STDIN_FILENO); //stdin to read pipe
-        close(fd[1]);
+        dup2(fd[0], STDIN_FILENO); //stdin of child 2 comes from read pipe
         close(fd[0]);
-        execvp(args2[0], args2);
-        perror("execvp failed");
+        close(fd[1]);
+        execvp(args2[0], args2); //execute second command
         exit(1);
     }
 
@@ -81,70 +96,39 @@ void execute_piped(char *line1, char *line2) {
     waitpid(pid1, NULL, 0);
     waitpid(pid2, NULL, 0);
 
-    free(args1); 
+    free(args1);
     free(args2);
 }
 
 int main() {
     CircularBuffer cb;
     buffer_init(&cb, MAX_LINE);
-
-    int c;
+    char line[MAX_LINE];
     int reachedEOF = 0;
 
-    while (reachedEOF==0) {
-        c = fgetc(stdin);
-        if (c == EOF) {
-            reachedEOF = 1;
-        } else {
-            buffer_push(&cb, c);
-        }
-
-        int line_size;
-        while ((line_size = buffer_size_next_element(&cb, '\n', reachedEOF)) > 0) {
-            char line[MAX_LINE];
-            for (int i = 0; i < line_size; i++) {
-                line[i] = buffer_pop(&cb);
+    while (read_next_line(&cb, line, &reachedEOF)) {
+        if (strcmp(line, "SINGLE") == 0) {
+            if (read_next_line(&cb, line, &reachedEOF)) {
+                execute_single(line, 1);
             }
-            line[line_size-1] = '\0'; // quitar \n
-
-            if (strcmp(line, "SINGLE") == 0) {
-                int size_next = buffer_size_next_element(&cb, '\n', reachedEOF);
-                char command[MAX_LINE];
-                for (int i = 0; i < size_next; i++){
-                    command[i] = buffer_pop(&cb);
-                } 
-                command[size_next-1] = '\0';
-                execute_single(command, 1);
+        } 
+        else if (strcmp(line, "CONCURRENT") == 0) {
+            if (read_next_line(&cb, line, &reachedEOF)) {
+                execute_single(line, 0);
             }
-            else if (strcmp(line, "CONCURRENT") == 0) {
-                int size_next = buffer_size_next_element(&cb, '\n', reachedEOF);
-                char command[MAX_LINE];
-                for (int i = 0; i < size_next; i++){
-                    command[i] = buffer_pop(&cb);
-                } 
-                command[size_next-1] = '\0';
-                execute_single(command, 0);
+        } 
+        else if (strcmp(line, "PIPED") == 0) {
+            char cmd1[MAX_LINE], cmd2[MAX_LINE];
+            if (read_next_line(&cb, cmd1, &reachedEOF) && 
+                read_next_line(&cb, cmd2, &reachedEOF)) {
+                execute_piped(cmd1, cmd2);
             }
-            else if (strcmp(line, "PIPED") == 0) {
-                int size1 = buffer_size_next_element(&cb, '\n', reachedEOF);
-                int size2 = buffer_size_next_element(&cb, '\n', reachedEOF);
-                char command1[MAX_LINE], command2[MAX_LINE];
-                for (int i = 0; i < size1; i++){
-                    command1[i] = buffer_pop(&cb); 
-                }
-                for (int i = 0; i < size2; i++){
-                    command2[i] = buffer_pop(&cb); 
-                } 
-                command1[size1-1] = '\0';
-                command2[size2-1] = '\0';
-                execute_piped(command1, command2);
-            }
-            else if (strcmp(line, "EXIT") == 0) {
-                reachedEOF = 1;
-            }
+        } 
+        else if (strcmp(line, "EXIT") == 0) {
+            break;
         }
     }
 
     buffer_deallocate(&cb);
+    return 0;
 }
